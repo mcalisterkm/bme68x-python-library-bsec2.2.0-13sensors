b238e6f12d766e45715e63401e1b53f5d72d3d82
Keith McAlister <mcalisterkm@gmail.com>
2023-01-17 16:57:46 +0000

    Add files via upload
    13 sensor version, API 4.4.7, BSEC 2.2.0.0

---
+++ bme68x-python-library-bsec2.2.0-13Sensors/sensor18.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Add back virtual sensor 18 BSEC_OUTPUT_COMPENSATED_GAS
+/usr/bin/cp ./BME68x-Sensor-API/bsec_datatypes.h  ./BSEC_2.2.0.0_Generic_Release_30052022/algo/normal_version/bin/RaspberryPi/PiThree_ArmV6
